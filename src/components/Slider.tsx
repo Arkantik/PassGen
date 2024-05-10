adab52f773d181525787090491a14566c170bb3a
Jeremy Dohin <arkantik.pro@gmail.com>
2024-05-10 19:13:33 +0200

    fix: 🐛 Fix of range input size
    Range input size was moving due to the password length number change

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -19,7 +19,7 @@ export default function Slider({ setPasswordLength }: SliderProps) {
     <div className="flex gap-2 md:gap-6 max-w-2xl w-full flex-wrap md:flex-nowrap">
       <label
         htmlFor="passwordLength"
-        className="flex gap-2 items-center whitespace-nowrap min-w-38"
+        className="flex gap-2 items-center whitespace-nowrap w-48"
       >
         Password length: <span className="font-semibold">{inputValue}</span>
       </label>
