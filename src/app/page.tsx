7a1f004342c923c50ee0a62bac62db92b7a6c5f2
Jeremy Dohin <arkantik.pro@gmail.com>
2024-05-11 10:27:34 +0200

    style: 💄 Update of overall style system

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,15 @@ export default function Home() {
   const [passwordLength, setPasswordLength] = useState(12);
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-4 md:p-12 overflow-hidden min-w-fit">
-      <div className="flex flex-col gap-2 mb-12 relative justify-center items-center">
-        <h1 className="text-6xl md:text-8xl font-bold text-center">PassGen</h1>
-        <p className="text-center text-zinc-300 text md:text-xl">
-          One click, endless security - craft a robust password lightning-fast
-        </p>
-        <div
-          className="w-full -z-20 absolute flex place-items-center 
+    <main className="grid grid-cols-1 min-h-screen p-4 overflow-hidden min-w-fit">
+      <div className="place-self-center place-content-end self-end">
+        <div className="flex flex-col gap-2 relative justify-center items-center text-center">
+          <h1 className="text-6xl md:text-8xl font-bold">PassGen</h1>
+          <p className="text-zinc-200 md:text-xl mb-12">
+            One click, endless security - craft a robust password lightning-fast
+          </p>
+          <div
+            className="w-full -z-20 absolute flex place-items-center 
         before:h-[300px] 
         before:md:h-[560px]
         before:w-full 
@@ -42,35 +43,36 @@ export default function Home() {
         after:via-[#0141ff] 
         after:opacity-40 
         sm:after:w-[440px]"
-        />
+          />
+        </div>
+        <div className="flex flex-col gap-6 w-full max-w-3xl justify-between items-center text-sm md:text-md">
+          <GeneratePassword
+            upperCaseEnabled={upperCaseEnabled}
+            numbersEnabled={numbersEnabled}
+            symbolsEnabled={symbolsEnabled}
+            passwordLength={passwordLength}
+          />
+          <Slider setPasswordLength={setPasswordLength} />
+          <ol className="flex flex-col md:flex-row gap-4 md:gap-6 justify-start md:justify-center max-w-2xl w-full">
+            <SwitchComponent
+              switchName="Uppercase"
+              enabled={upperCaseEnabled}
+              setEnabled={setUpperCaseEnabled}
+            />
+            <SwitchComponent
+              switchName="Numbers"
+              enabled={numbersEnabled}
+              setEnabled={setNumbersEnabled}
+            />
+            <SwitchComponent
+              switchName="Symbols"
+              enabled={symbolsEnabled}
+              setEnabled={setSymbolsEnabled}
+            />
+          </ol>
+        </div>
       </div>
-      <div className="flex flex-col gap-6 w-full max-w-3xl justify-between items-center text-sm md:text-md">
-        <GeneratePassword
-          upperCaseEnabled={upperCaseEnabled}
-          numbersEnabled={numbersEnabled}
-          symbolsEnabled={symbolsEnabled}
-          passwordLength={passwordLength}
-        />
-        <Slider setPasswordLength={setPasswordLength} />
-        <ol className="flex flex-col md:flex-row gap-4 md:gap-6 justify-start md:justify-center max-w-2xl w-full">
-          <SwitchComponent
-            switchName="Uppercase"
-            enabled={upperCaseEnabled}
-            setEnabled={setUpperCaseEnabled}
-          />
-          <SwitchComponent
-            switchName="Numbers"
-            enabled={numbersEnabled}
-            setEnabled={setNumbersEnabled}
-          />
-          <SwitchComponent
-            switchName="Symbols"
-            enabled={symbolsEnabled}
-            setEnabled={setSymbolsEnabled}
-          />
-        </ol>
-      </div>
-      <div className="fixed bottom-0 flex justify-center md:justify-end items-center w-full md:px-12 pb-4">
+      <div className="flex w-full place-items-end place-content-center pt-12 md:pt-0">
         <span className="flex gap-2 text-sm">
           Made by Jérémy Dohin
           <span>|</span>
